787f87e39afd869827637648a6527a30d687c946
Reno <singularitti@outlook.com>
2022-08-19 02:46:11 -0400

    Reformat build.jl with Blue style

--- build.jl
+++ build.jl
@@ -1,5 +1,13 @@
 using PkgTemplates
-using PkgTemplates: Plugin, default_file, gen_file, render_file, combined_view, tags, @plugin, @with_kw_noshow
+using PkgTemplates:
+    Plugin,
+    default_file,
+    gen_file,
+    render_file,
+    combined_view,
+    tags,
+    @plugin,
+    @with_kw_noshow
 import PkgTemplates: view, hook
 
 default_dir() = joinpath(pwd(), "templates")
@@ -8,14 +16,14 @@ default_dir() = joinpath(pwd(), "templates")
     readme_md::String = joinpath(default_dir(), "README.md")
     installation_md::String = joinpath(default_dir(), "docs", "src", "installation.md")
     contributing_md::String = joinpath(default_dir(), "docs", "src", "contributing.md")
-    troubleshooting_md::String = joinpath(default_dir(), "docs", "src", "troubleshooting.md")
+    troubleshooting_md::String = joinpath(
+        default_dir(), "docs", "src", "troubleshooting.md"
+    )
 end
 
-view(::MyDocs, t::Template, pkg::AbstractString) = Dict(
-    "USER" => t.user,
-    "PKG" => pkg,
-    "jl" => "1.6.7"
-)
+function view(::MyDocs, t::Template, pkg::AbstractString)
+    return Dict("USER" => t.user, "PKG" => pkg, "jl" => "1.6.7")
+end
 
 function hook(p::MyDocs, t::Template, pkg_dir::AbstractString)
     pkg = basename(pkg_dir)
@@ -32,11 +40,20 @@ t = Template(;
     plugins=[
         License(),
         Git(),
-        GitHubActions(; x86=true, osx=true, windows=true, extra_versions=["1.0", "1.3", "1.4", "1.6", "1.7", "nightly"]),
+        GitHubActions(;
+            x86=true,
+            osx=true,
+            windows=true,
+            extra_versions=["1.0", "1.3", "1.4", "1.6", "1.7", "nightly"],
+        ),
         Codecov(),
         Documenter{GitHubActions}(),
         AppVeyor(; x86=true, extra_versions=["1.0", "1.1", "1.5", "nightly"]),
-        CirrusCI(; extra_versions=["1.1", "1.4", "1.5", "nightly"], image="freebsd-12-1-release-amd64", file="templates/cirrus.yml"),
+        CirrusCI(;
+            extra_versions=["1.1", "1.4", "1.5", "nightly"],
+            image="freebsd-12-1-release-amd64",
+            file="templates/cirrus.yml",
+        ),
         GitLabCI(; extra_versions=["1.2", "1.3", "1.6", "1.7"]),
         CompatHelper(),
         RegisterAction(),
@@ -46,5 +63,5 @@ t = Template(;
         Documenter(; index_md="templates/docs/src/index.md"),
         !Readme,
         MyDocs(),
-    ]
+    ],
 )
